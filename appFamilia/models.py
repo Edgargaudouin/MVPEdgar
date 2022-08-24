67e27e620e64ca17fd53681bb70f32664f207bcf
Edgar Romero <edgar.gaudouin.dev@gmail.com>
2022-08-24 17:44:58 -0300

    Con el uso de cargador loader importado de django.template en views logré generar la conexión con la plantilla

--- appFamilia/models.py
+++ appFamilia/models.py
@@ -6,6 +6,6 @@ class Familiar(models.Model):
 
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
-    parentezco = models.CharField(max_length=40)
+    parentesco = models.CharField(max_length=40)
     edad = models.IntegerField()
     fecha_nacimiento = models.DateField()
--- appFamilia/views.py
+++ appFamilia/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render, HttpResponse
 from appFamilia.models import Familiar
-from django.template import Template, Context
+from django.template import loader
+
 # Create your views here.
 
 def familia(self):
-    familiar1 = Familiar( nombre = "Edgar", apellido = "Romero", parentezco = "Padre", edad = 70, fecha_nacimiento = "1952-01-24" )
+    familiar1 = Familiar( nombre = "Edgar", apellido = "Romero", parentesco = "Padre", edad = 70, fecha_nacimiento = "1952-01-24" )
     familiar1.save()
 
-    familiar2 = Familiar( nombre = "France", apellido = "Gaudouin", parentezco = "Madre", edad = 60, fecha_nacimiento = "1962-02-15" )
+    familiar2 = Familiar( nombre = "France", apellido = "Gaudouin", parentesco = "Madre", edad = 60, fecha_nacimiento = "1962-02-15" )
     familiar2.save()
 
-    familiar3 = Familiar( nombre = "Isamar", apellido = "Romero", parentezco = "Hermana", edad = 26, fecha_nacimiento = "1996-08-11" )
+    familiar3 = Familiar( nombre = "Isamar", apellido = "Romero", parentesco = "Hermana", edad = 26, fecha_nacimiento = "1996-08-11" )
     familiar3.save()
 
-    diccionario = {"familiares": [familiar1, familiar2, familiar3]}
-    miHtml = open('C:/Users/Edgar/Desktop/MVPEdgar/Proyecto/plantillas/plantilla1.html')
-    plantilla = Template(miHtml.read())
-    miContexto = Context(diccionario)
-    documento = plantilla.render(miContexto)
+    plantilla = loader.get_template('plantilla1.html')
+    familiares = {'familiares' : [familiar1, familiar2, familiar3]}
+    documento = plantilla.render(familiares)
     return HttpResponse(documento)
+
+
 
 
 
